30d26fb3da86a522215921171cf9adc321e331ab
jordan-christensen <jkchristensen36@gmail.com>
2024-09-18 15:44:38 -0700

    App and JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,6 +33,10 @@ topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
+document.getElementById("alert-button").addEventListener("click", function() {
+  alert("Button clicked!");
+});
+
 // connect if there are any LiveViews on the page
 liveSocket.connect()
 
